e9319dc78be8272b84f00e1616633aff01c15ef6
Caleb Yaghoubi <Caleby315@gmail.com>
2025-11-12 17:43:39 -0500

    added first user management endpoint and logic to connect to a dockerize postgres database

--- backend/readme.md
+++ backend/readme.md
@@ -15,3 +15,31 @@ docker run --name some-postgres -e POSTRGRES_PASSWORD=mysecretpassword -p 5432:5
 -p 5432:5432: Maps port 5432 on your local machine to port 5432 inside the container, allowing external connections.\
 -d: Runs the container in detached mode, meaning it runs in the background.\
 postgres: Specifies the Docker image to use.
+
+To run uvicorn
+```bash
+    uvicorn server:app --host 127.0.0.1 --port 8000 --reload 
+```
+
+Connect in terminal to postgres
+```bash
+psql -h localhost -U postgres -p 5432 -d user_database
+```
+
+- user_database is the database
+- users is the table
+
+Used to create the table
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    username VarChar(50) UNIQUE NOT NULL,
+    firstname VarChar(50) NOT NULL,
+    lastname VarChar(50) NOT NULL,
+    email VarChar(50) UNIQUE NOT NULL,
+    isverified BOOLEAN NOT NULL,
+    createdat TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+Adding a new user 
+INSERT INTO users (username, firstname, lastname, email, isverified)
+VALUES ('testuser', 'Caleb', 'Yaghoubi', 'test@email.com', true);
--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,74 @@
 from fastapi import FastAPI
+import json
+from contextlib import asynccontextmanager
+import psycopg2
 
-app = FastAPI()
 
 
-@app.get("/api")
-async def read_root():
-    return {"message": "Hello from FastAPI and Uvicorn!"}
 
-@app.get("/api/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id, "message": f"This is item {item_id}"}
+conn = None #postgres connection
+cur = None #postgres terminal cursor
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic: initialize the database connection
+    global conn
+    global cur
+    # read connection parameters, for security setup a config file for db params
+    #params = config()
+
+    # connect to the PostgreSQL server
+    print('Connecting to the PostgreSQL database...')
+    conn = psycopg2.connect(dbname="user_database",
+                            user="postgres",
+                            password="password",
+                            host="localhost")
+    
+    # create a cursor
+    cur = conn.cursor()
+    print("Database connected!")
+    yield
+    # Shutdown logic: close the database connection
+    print("Closing database connection...")
+    if cur is not None:
+        cur.close()
+        print('Cursor closed')
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
+    
+
+app = FastAPI(lifespan=lifespan)
+
+
+
+#User CRUD OPs
+@app.get('/api/users/') # Get User by Id
+async def get_user(userId: int = 0):
+    #incoming: userId
+    #outgoing: username, firstname, lastname, email
+    error  = ''
+
+    if userId <= 0: #Validate User
+        error = 'UserId is Not Specified or Invalid'
+        return {'error' : error}
+    
+    query = 'SELECT * FROM users WHERE user_id = %s;'
+    cur.execute(query, (str(userId)))
+    results = cur.fetchone()
+    
+    if results is not None:
+        user_id, username, firstname, lastname, email, _, _ = results
+        return {'user_id': user_id, 'username': username, 'firstname': firstname, 'lastname':lastname, 'email':email,'error': error}
+    else:
+        error = f'No Users Found in Database'
+        return {'error': error}
+    
+
+@app.post('/api/users/insert/')
+async def insert_user():
+    #incoming: username, firstname, lastname, email, password
+    #outgoing: UserId->Now, JWT->later
+    error  = ''
+    return {'error': error}
+
