adc594ecbfad088d6f2d805996b90522738af9d9
Edwin Gabriel <65310253+Edwinvilla72@users.noreply.github.com>
2025-12-04 16:06:10 -0500

    Continued progress with agent and simple dom extraction

--- browser-iteraction/practice/agentic-browser-loop-test.py
+++ browser-iteraction/practice/agentic-browser-loop-test.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import asyncio
 import pyautogui
-from playwright.async_api import async_playwright
+from playwright.async_api import async_playwright, Playwright
 import random
 from pathlib import Path
 # from agentic-tools import * # for tools for the agent to call for future implementation
@@ -17,7 +17,7 @@ client = OpenAI()
 
 system_prompt = """
 You are a web browsing agent. You will recieve input from a user who would like to perform an action on the internet. 
-Listen to the user's input and based on their request, generate a response that you think reflects the best next 
+Listen to the user's input and based on their request, generate a response that you think reflects the best next step within the web browser using available tools.
 """
 
 # === type in random intervals function 
@@ -26,7 +26,7 @@ async def type_in_random_intervals(message):
     for ch in message:
         pyautogui.typewrite(ch)
         await asyncio.sleep(random.uniform(0.02, 0.2))
-# ===
+
 
 # === generate llm input function ===
 # generates response to the current page and state through OpenAI's gpt o4 mini model API
@@ -39,11 +39,45 @@ def generate_llm_input(current_page):
     print(response.output_text)
     return response.output_text
 
+
 # === (driver) main function ===
 # asks the user for initial input
-def main(): 
+async def main():
+
+    # take initial user input and send it to the model
+    # user_input = input("What would you like out of this browsing session?\nYour input: ")
+
+    # the site which will change over time as we continue to navigate the internet
+    web_url = "https://google.com"
     
-    # take user input and send it to the model
-    user_input = input("What would you like out of this browsing session?\nYour input: ")
+    ## generate initial input to bring us to the first page on the browser 
+    # response = client.responses.create(
+    #     model = "o4-mini-2025-04-16", 
+    #     input = [
+    #         # system input
+    #         {"role": "system", "content": system_prompt},
+    #         # user input
+    #         {"role": "user", "content": user_input}
+    #     ]
+    # )
     
-    
+    # start browser off at Google.com
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await page.goto(web_url)
+
+
+        # based on the current first page we are on, generate a list of actions (in order from first to last) that will bring us to the next step of completion
+        #? HOW ARE WE SENDING THIS WITHOUT COLLECTING DOM INFO FROM BI?
+
+        # for now, extract compressed DOM 
+        print("Getting DOM elements from current page...")
+        ax_tree = await page.accessibility.snapshot(root=None, interesting_only=True)
+        print(ax_tree)
+        print(f"DOM recieved from {web_url}!")
+
+        # once DOM is recieved, 
+
+if __name__ == "__main__": 
+    asyncio.run(main())
