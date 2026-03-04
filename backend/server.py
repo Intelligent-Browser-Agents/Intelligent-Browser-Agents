8bd07ab3919f0c588e20bb8a8f48804b4b4edeb1
Caleb Yaghoubi <Caleby315@gmail.com>
2026-03-03 21:13:29 -0500

    fixed ip addresses of server

--- backend/server.py
+++ backend/server.py
@@ -87,7 +87,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://100.49.61.97"], # Your React URL
+    allow_origins=["http://0.0.0.0"], # Your React URL
     allow_credentials=True,
     allow_methods=["*"], # This allows POST, OPTIONS, etc.
     allow_headers=["*"],
