7107cc668a89c28fcad03d39ec4abcd82018dffd
Caleb Yaghoubi <Caleby315@gmail.com>
2026-03-03 21:54:39 -0500

    fixed fake code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // This is the "nickname" Vite looks for
-      '/api': {
-        target: 'http://1.1.1.1:8000', // Your EC2 Backend
-        changeOrigin: true,
-        // Optional: If your FastAPI routes ALREADY start with /api, 
-        // you don't need to rewrite anything. 
-        // If they DON'T, use the rewrite line below:
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  plugins: [react()]
 })
