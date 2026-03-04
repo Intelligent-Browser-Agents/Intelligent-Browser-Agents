57ef2d143db2b964f73a4619fe17c521ff3eb910
Edwin Gabriel <65310253+Edwinvilla72@users.noreply.github.com>
2026-03-03 22:53:04 -0500

    - Created new verify user endpoint - Created new verify user function to dashboard.jsx - New bool value to determine whether we verified the user's identity or not - if not, displays prompt for user to verify - if so, displays credentials (right now it does not, will work on in next session)

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Register.css"; // optional, you can style separately
 
 export default function Register() {
   const navigate = useNavigate();
 
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Email, setEmail] = useState("");
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect (() => {
+    setMessage('');
+  })
+
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const headers = {
+      'Content-Type': 'application/json' // Include the token in the Authorization header
+    };
+
+    const response = await fetch('/api/users/insert/', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify({ username: Username, firstname: FirstName, lastname: LastName, email: Email, password: Password }), // Sending raw JSON
+    });
+    
+    const data = await response.json();
+    if (data.userId) {
+      return navigate('/');
+    }
+    setMessage('Registration failed. ' + (data.error || 'Unknown error') + '. Please try again.');
+    return navigate("/register");
+  };
+
   return (
     <div className="register-page">
       <div className="registe-container">
         <h1>Create an Account</h1>
 
-        <form className="register-form">
-          <input type="text" placeholder="Username" className="register-input" />
-          <input type="email" placeholder="Email" className="register-input" />
-          <input type="password" placeholder="Password" className="register-input" />
-          <button onClick={() => navigate("/")} type="submit" className="register-button"  >Register</button>
+        <form onSubmit={handleRegister} className="register-form">
+          <input type="text" placeholder="First Name" className="register-input" onChange={(e) => setFirstName(e.target.value)} />
+          <input type="text" placeholder="Last Name" className="register-input" onChange={(e) => setLastName(e.target.value)} />
+          <input type="text" placeholder="Username" className="register-input" onChange={(e) => setUsername(e.target.value)} />
+          <input type="email" placeholder="Email" className="register-input" onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="Password" className="register-input" onChange={(e) => setPassword(e.target.value)} />
+          {message && (
+            <p style={{ color: 'red', fontSize: '14px', marginTop: '10px' }}>
+              {message}
+            </p>
+          )}
+          <button type="submit" className="register-button"  >Register</button>
         </form>
 
         <div className="back-to-login">
