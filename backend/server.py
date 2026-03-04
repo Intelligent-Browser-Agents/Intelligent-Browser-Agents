b0c6f9b332eee37ac6ee45605f06ab3821600697
Caleb Yaghoubi <Caleby315@gmail.com>
2026-03-03 21:31:35 -0500

    testing server issues

--- backend/server.py
+++ backend/server.py
@@ -87,7 +87,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://0.0.0.0"], # Your React URL
+    allow_origins=["*"], # Your React URL
     allow_credentials=True,
     allow_methods=["*"], # This allows POST, OPTIONS, etc.
     allow_headers=["*"],
